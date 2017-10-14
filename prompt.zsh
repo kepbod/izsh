051c2179661d6a171714e805c91e4d09df7d795a
Xiao-Ou Zhang <kepbod@gmail.com>
2017-10-14 18:09:49 -0400

    modify prompt

--- prompt.zsh
+++ prompt.zsh
@@ -1,10 +1,10 @@
 # order
 SPACESHIP_PROMPT_ORDER=(
+    dir           # Current directory section
     user          # Username section
     host          # Hostname section
-    dir           # Current directory section
-    git           # Git section (git_branch + git_status)
     conda         # conda virtualenv section
+    git           # Git section (git_branch + git_status)
     exec_time     # Execution time
     line_sep      # Line break
     vi_mode       # Vi-mode indicator
@@ -16,14 +16,23 @@ SPACESHIP_PROMPT_SYMBOL="❯"
 
 # user
 SPACESHIP_USER_PREFIX=""
+SPACESHIP_USER_SUFFIX=""
+
+# host
+SPACESHIP_HOST_PREFIX="@"
 
 # git
+SPACESHIP_GIT_PREFIX=""
 SPACESHIP_GIT_SYMBOL=""
 SPACESHIP_GIT_BRANCH_PREFIX="$SPACESHIP_GIT_SYMBOL"
+SPACESHIP_GIT_STATUS_PREFIX=""
+SPACESHIP_GIT_STATUS_SUFFIX=""
+
+# conda
+SPACESHIP_CONDA_PREFIX=""
 
 # exec_time
 SPACESHIP_EXEC_TIME_PREFIX=""
-
 
 # vi-mode
 SPACESHIP_VI_MODE_INSERT="❯"
